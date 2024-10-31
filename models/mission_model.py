be47e2a00fb361ed3c3fed2a80c5730bfc35f907
shalom bergman <shalom195295@gmail.com>
2024-10-31 16:58:03 +0200

    טיפול ב relationship

--- models/mission_model.py
+++ models/mission_model.py
@@ -1,17 +1,21 @@
-from flask import session
-from sqlalchemy import Column, Integer, String, Date, Table, ForeignKey, Index,Float
-from based.base import Base
+
+from sqlalchemy import Column, Integer, Date, Float, ForeignKey
 from sqlalchemy.orm import relationship
+
+from based.base import Base
+
 
 
 class MissionModel(Base):
     __tablename__ = 'missions'
-    id = Column(Integer, primary_key=True)
+    mission_id  = Column(Integer, primary_key=True)
     mission_date = Column(Date)
-    airborne_aircrat = Column(Float)
-    attacking_aircrat = Column(Float)
-    bomding_aircrat = Column(Float)
+    airborne_aircraft  = Column(Float)
+    attacking_aircraft = Column(Float)
+    bombing_aircraft = Column(Float)
     aircraft_returned = Column(Float)
     aircraft_failed = Column(Float)
     aircraft_damaged = Column(Float)
     aircraft_lost = Column(Float)
+
+    targets = relationship('TargetModel', back_populates='mission')
