e20318b404eefbcbaa04a646b1ba7d7ef05b2181
shalom bergman <shalom195295@gmail.com>
2024-10-31 10:35:49 +0200

    create a TargetTypeModel file

---
+++ models/targettype_model.py
@@ -0,0 +1,10 @@
+from flask import session
+from sqlalchemy import Column, Integer, String, Date, Table, ForeignKey, Index,Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+class TargetTypeModel(Base):
+    __tablename__ = 'targettypes'
+    target_type_id = Column(Integer, primary_key=True)
+    target_type_name = Column(String)
