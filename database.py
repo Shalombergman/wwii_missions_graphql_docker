c14ce09869fcbabac816224feddcf102491ba128
shalom bergman <shalom195295@gmail.com>
2024-10-31 17:01:19 +0200

    הסרת db_init

--- database.py
+++ database.py
@@ -1,17 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-
 from based.base import Base
 from models import *
 
 
 #todo: can be converted to env variable via os.environ.get('DB_URL')
-connection_url = "postgresql://admin:1234@localhost:5437/missions_db"
+connection_url = "postgresql://admin:admin@psql-wwii:5432/missions_db"
 engine = create_engine(connection_url, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
 
-def init_db():
-    import models
-    Base.metadata.create_all(bind=engine)
