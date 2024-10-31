d1c88dcc2e39d6657f59f86f8b5f0de446bab5af
shalom bergman <shalom195295@gmail.com>
2024-10-31 16:55:34 +0200

    תיקון היחסים בין הטבלאות והמודליםם

--- models/targettype_model.py
+++ models/targettype_model.py
@@ -1,10 +1,13 @@
-from flask import session
+
 from sqlalchemy import Column, Integer, String, Date, Table, ForeignKey, Index,Float
-from based.base import Base
 from sqlalchemy.orm import relationship
+
+from based.base import Base
+
 
 
 class TargetTypeModel(Base):
     __tablename__ = 'targettypes'
     target_type_id = Column(Integer, primary_key=True)
     target_type_name = Column(String)
+    targets_types = relationship('TargetModel', back_populates='target_type')
